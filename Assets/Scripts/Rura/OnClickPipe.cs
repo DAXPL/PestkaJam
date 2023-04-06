22b5ac96be8d2d1e16ac2bc134d19abce67893b7
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-04-07 00:07:11 +0200

    Pipes connect on click if color is correct

---
+++ Assets/Scripts/Rura/OnClickPipe.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickPipe : MonoBehaviour {
+    public List<LineRenderer> pipes = new List<LineRenderer>();
+
+
+    public void ConnectPipes(GameObject pipe) {
+        if (pipe.GetComponent<PipeInfo>().used) {
+            return;
+        }
+
+        pipe.GetComponent<PipeInfo>().used = true;
+
+        pipes.Add(pipe.GetComponent<LineRenderer>());
+
+        if (pipes.Count == 2) {
+            pipes[0].SetPosition(0, pipes[0].transform.position);
+            pipes[0].SetPosition(1, pipe.transform.position);
+
+            pipes[0].startColor = pipes[0].GetComponent<PipeInfo>().color;
+            pipes[0].endColor = pipes[0].GetComponent<PipeInfo>().color;
+            
+            StartCoroutine(CheckColors(pipe));
+        }
+    }
+
+    IEnumerator CheckColors(GameObject pipe) {
+        yield return new WaitForSeconds(2);
+
+        if (pipes[1].GetComponent<PipeInfo>().color == pipes[0].GetComponent<PipeInfo>().color) {
+            pipes[0].GetComponent<Button>().interactable = false;
+            pipes[1].GetComponent<Button>().interactable = false;
+            pipes.Clear();
+            yield break;
+        }
+
+        pipes[0].SetPosition(0, Vector3.zero);
+        pipes[0].SetPosition(1, Vector3.zero);
+
+        pipes[0].GetComponent<PipeInfo>().used = false;
+        pipes[1].GetComponent<PipeInfo>().used = false;
+
+        pipes.Clear();
+    }
+}
