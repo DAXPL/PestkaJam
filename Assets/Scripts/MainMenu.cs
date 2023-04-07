723322f32fda251023b1c4c6676eeffb25a1c95c
Miłosz Klim <klim.milosz@gmail.com>
2023-04-07 11:02:46 +0200

    Game loop babe

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int unlockedLevels = 0;
+    public Button[] levelButtons;
+    private void Start()
     {
-        
-    }
+        unlockedLevels = PlayerPrefs.GetInt("Levels");
+        Debug.Log($"Levels: {unlockedLevels}");
 
-    // Update is called once per frame
-    void Update()
+        for (int i=0;i<levelButtons.Length;i++)
+        {
+            levelButtons[i].interactable = (i <= unlockedLevels);
+        }
+    }
+    public void LoadLevel(int level)
     {
-        
+        SceneManager.LoadScene(level);
     }
 }
